9203b5592682817f5d54c8e1d498f622d53d1554
Piyush rai <31138966+Piyushrai558@users.noreply.github.com>
2022-08-19 01:52:23 +0530

    Update BlogApiApplicationTests.java

--- src/test/java/com/piyushrai/blog/BlogApiApplicationTests.java
+++ src/test/java/com/piyushrai/blog/BlogApiApplicationTests.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.piyushrai.blog.repositories.UserRepo;
-
 @SpringBootTest
 class BlogApiApplicationTests {
 	
